ed64219a10e4e97b796ab0ec634fe0f9a05b7fba
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-17 11:15:32 -0700

    update BridgesController.cs to include methods for data access and manipulation
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- Controllers/BridgesController.cs
+++ Controllers/BridgesController.cs
@@ -1,21 +1,59 @@
-using System.ComponentModel.DataAnnotations;
-using System;
+using Architecture.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Bridges.Models
+namespace Architecture.Controllers
 {
-    public class Bridge
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BridgesController : ControllerBase
+  {
+    private ArchitectureContext _db;
+
+    public BridgesController(ArchitectureContext db)
     {
-      public int BridgeId { get; set; }
-        [Required]
-        [StringLength(20)]
-        public string Name { get; set; }
-        [Required]
-        public string Country { get; set; }
-        [Required]
-        public string City { get; set; }
-        [Required]
-        public DateTime DateOfConstruction { get; set; }
-        [Required]
-        public string Architect { get; set; }
+      _db = db;
     }
+
+    // GET api/Bridges
+    [HttpGet]
+    public ActionResult<IEnumerable<Bridge>> Get()
+    {
+      var query = _db.Bridges.AsQueryable();
+
+      return query.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Bridge> Get(int id)
+    {
+        return _db.Bridges.FirstOrDefault(entry => entry.BridgeId == id);
+    }
+
+    // PUT api/Bridges/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Bridge bridge)
+    {
+        bridge.BridgeId = id;
+        _db.Entry(bridge).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
+    // POST api/Bridges
+    [HttpPost]
+    public void Post([FromBody] Bridge bridge)
+    {
+      _db.Bridges.Add(bridge);
+      _db.SaveChanges();
+    }
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var bridgeToDelete = _db.Bridges.FirstOrDefault(entry => entry.BridgeId == id);
+      _db.Bridges.Remove(bridgeToDelete);
+      _db.SaveChanges();
+    }
+  }
 }
