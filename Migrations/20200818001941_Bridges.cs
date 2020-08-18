b6995cc3d25fbd3f1a24b211251b1b8b2145fd57
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-17 17:20:44 -0700

    add 5 new bridges to API

---
+++ Migrations/20200818001941_Bridges.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Architecture.Migrations
+{
+    public partial class Bridges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Bridges",
+                columns: new[] { "BridgeId", "Architect", "City", "Country", "EndDate", "Name", "Span", "StartDate" },
+                values: new object[,]
+                {
+                    { 2, "John Bradfield", "Sydney", "AUS", new DateTime(1932, 1, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sydney Harbour", 1149, new DateTime(1928, 7, 28, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "Satoshi Kashima", "Kobe, Iwaya", "JPN", new DateTime(1998, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Akashi Kaikyo", 3911, new DateTime(1988, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, "Dr Michel Virlogeux", "Millau", "FRA", new DateTime(2004, 12, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Millau Viaduct", 2460, new DateTime(2001, 10, 16, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, "John Augustus Roebling", "Brooklyn, Manhattan", "USA", new DateTime(1883, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Brooklyn", 18334, new DateTime(1869, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Bridges",
+                keyColumn: "BridgeId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Bridges",
+                keyColumn: "BridgeId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Bridges",
+                keyColumn: "BridgeId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Bridges",
+                keyColumn: "BridgeId",
+                keyValue: 5);
+        }
+    }
+}
--- Models/ArchitectureContext.cs
+++ Models/ArchitectureContext.cs
@@ -16,8 +16,11 @@ namespace Architecture.Models
         {
             builder.Entity<Bridge>()
                 .HasData(
-                    new Bridge { BridgeId = 1, Name = "Golden Gate", Country = "USA", City = "San Francisco", StartDate = new DateTime(1933, 1, 5), EndDate = new DateTime(1937, 4, 19), Architect = "Joseph Strauss", Span = 1280  }
-
+                    new Bridge { BridgeId = 1, Name = "Golden Gate", Country = "USA", City = "San Francisco", StartDate = new DateTime(1933, 1, 5), EndDate = new DateTime(1937, 4, 19), Architect = "Joseph Strauss", Span = 1280 },
+                    new Bridge { BridgeId = 2, Name = "Sydney Harbour", Country = "AUS", City = "Sydney", StartDate = new DateTime(1928, 7, 28), EndDate = new DateTime(1932, 1, 18), Architect = "John Bradfield", Span = 1149 },
+                    new Bridge { BridgeId = 3, Name = "Akashi Kaikyo", Country = "JPN", City = "Kobe, Iwaya", StartDate = new DateTime(1988, 1, 1), EndDate = new DateTime(1998, 1, 1), Architect = "Satoshi Kashima", Span = 3911 },
+                    new Bridge { BridgeId = 4, Name = "Millau Viaduct", Country = "FRA", City = "Millau", StartDate = new DateTime(2001, 10, 16), EndDate = new DateTime(2004, 12, 16), Architect = "Dr Michel Virlogeux", Span = 2460 },
+                    new Bridge { BridgeId = 5, Name = "Brooklyn", Country = "USA", City = "Brooklyn, Manhattan", StartDate = new DateTime(1869, 1, 1), EndDate = new DateTime(1883, 1, 1), Architect = "John Augustus Roebling", Span = 18334 }
                 );
         }
     }
